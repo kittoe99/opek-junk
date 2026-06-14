a2759c430cb84713df7afb70190351f6867725bb
Developer <dev@opekjunkremoval.com>
2026-06-14 12:09:43 -0600

    feat: remove duplicate Process section from homepage

--- App.tsx
+++ App.tsx
@@ -4,7 +4,6 @@ import { Navbar } from './components/Navbar';
 import { Hero } from './components/Hero';
 import { TrustBadges } from './components/TrustBadges';
 import { Services } from './components/Services';
-import { Process } from './components/Process';
 import { ServiceArea } from './components/ServiceArea';
 import { Footer } from './components/Footer';
 import { QuotePage } from './components/QuotePage';
@@ -68,7 +67,6 @@ function HomePage() {
       <TrustBadges />
       <Services />
       <CharityBanner />
-      <Process />
       <ProcessEditorial />
       <WhatWeHaul />
       <FullServiceSection />
